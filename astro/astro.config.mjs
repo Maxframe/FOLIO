3a3b677760a72cafb1ee2e4e1e4e42ed13c613df
Maxframe <wrnerm@gmail.com>
2024-01-11 21:53:49 +0100

    remove vercel adapter

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,14 +1,7 @@
 import { defineConfig } from "astro/config";
 import { sanityIntegration as sanity } from "@narration-sd/sanity-astro";
-import vercel from "@astrojs/vercel/static";
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   integrations: [
     sanity({
       projectId: "lctqiis2",
