9798bf9be4ccd591eba378303db08763ab504dbe
Maxframe <wrnerm@gmail.com>
2024-03-12 13:14:20 +0100

    env and astro upgrade

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import { sanityIntegration as sanity } from "@sanity/astro";
+import { loadEnv } from "vite";
+
+const { SANITY_PROJECT_ID } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sanity({
-      projectId: "lctqiis2",
+      projectId: SANITY_PROJECT_ID,
       dataset: "production",
       apiVersion: "v2021-03-25",
       useCdn: false,
