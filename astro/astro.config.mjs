b847812ef2a005793b239759cffa3f7d6943a707
Maxframe <wrnerm@gmail.com>
2024-03-12 13:24:18 +0100

    removed env as unnecessary

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from "astro/config";
 import { sanityIntegration as sanity } from "@sanity/astro";
-import { loadEnv } from "vite";
-
-const { SANITY_PROJECT_ID } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sanity({
-      projectId: SANITY_PROJECT_ID,
+      projectId: "lctqiis2",
       dataset: "production",
       apiVersion: "v2021-03-25",
       useCdn: false,
