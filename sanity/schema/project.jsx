935c9f8984da0d024c87d75188f0d713d04c95f1
Maxframe <wrnerm@gmail.com>
2024-06-03 19:20:09 +0200

    feat; added subtitle and various improvements

--- sanity/schema/project.jsx
+++ sanity/schema/project.jsx
@@ -41,6 +41,13 @@ export default defineType({
       validation: (Rule) => [Rule.required()],
     }),
     defineField({
+      name: "subtitle",
+      title: "Subtitle",
+      type: "string",
+      group: "main",
+      validation: (Rule) => [Rule.required()],
+    }),
+    defineField({
       name: "thumbnailImage",
       title: "Thumbnail Image",
       type: "figure",
